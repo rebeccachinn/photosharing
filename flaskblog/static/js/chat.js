9207c61f42fc9fab160761c0c6d438440bfc6d55
rebecca_chinn <rc3003@barnard.edu>
2018-11-17 10:18:10 -0500

    added chat functionality for one large room

--- flaskblog/static/js/chat.js
+++ flaskblog/static/js/chat.js
@@ -1,20 +1,20 @@
-// $(document).ready(function() {
-// 	var socket = io.connect('http://127.0.0.1:5000');
-// 	socket.on('connect', function() {
-// 		// socket.send('User has connected!');
-// 		console.log("User connected");
-// 	});
-// 	socket.on('message', function(msg) {
-// 		// $("#messages").append('<li>' + msg +'</li>');
-// 		location.reload()
-// 		console.log('Received message');
-// 	});
+$(document).ready(function() {
+	var socket = io.connect('http://127.0.0.1:5000');
+	socket.on('connect', function() {
+		// socket.send('User has connected!');
+		console.log("User connected");
+	});
+	socket.on('message', function(msg) {
+		// $("#messages").append('<li>' + msg +'</li>');
+		location.reload()
+		console.log('Received message');
+	});
 
-// 	$('#sendbutton').on('click', function() {
-// 		socket.send($('#myMessage').val());
-// 		$('#myMessage').val('');
-// 		console.log("message sent!")
-// 	});
+	$('#sendbutton').on('click', function() {
+		socket.send($('#myMessage').val());
+		$('#myMessage').val('');
+		console.log("message sent!")
+	});
 
 
 	// const realFileBtn = $("#real-file");
@@ -31,4 +31,4 @@
 	// 		customTxt.html(realFileBtn.val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]);
 	// 	}
 	// })
-// });
+});
